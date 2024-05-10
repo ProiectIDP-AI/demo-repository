a093ee8ecd2623ca9b5239f577014e7f1a30d7dd
Andrei-Ionuț IONESCU <andrei.ionescu1411@stud.acs.upb.ro>
2024-05-10 20:30:08 +0300

    fsd

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, welcome to my Flask app test!"
+    return "Hello, welcome to my Flask app test2!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
